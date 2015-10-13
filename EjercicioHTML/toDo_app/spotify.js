b03b84dad95a6e61342c6659a1fc6998cfe194e4
Maria Jose Montero <marimontero_19@hotmail.com>
2015-10-13 18:07:00 +0200

    Clase Lunes

--- EjercicioHTML/toDo_app/spotify.js
+++ EjercicioHTML/toDo_app/spotify.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 			url: searchUrl,
 			success: function(response){
 				var artistsInfo = response.artists.items;
+				$('#artist-info').empty();
+				$("#search-input").val('');
 				printArtists(artistsInfo);
 			},
 			error: function(error){
@@ -16,10 +18,32 @@ $(document).ready(function(){
 		});
 	}
 
+	function getAlbums(event) {
+		event.preventDefault();
+		var $artistSelected = $(event.currentTarget);
+		var albumsUrl = $artistSelected.attr('data-url');
+		$.ajax({
+			type: "GET",
+		});
+	};
+
+			url: albumsUrl,
+			success: function(response){
+				$('#artist-albums').empty();
+				printAlbums(response.items);
+			},
+			error: function(error){
+				console.log(error);
+			}
 	function createElement(artistInfo){
 		//console.log(artistInfo.name);
 		var $artistItem = $('<li>');
-		$artistItem.html(artistInfo.name);
+		var $artistLink = $('<a href="#">').addClass('artist-result');
+		var albumsUrl = artistInfo.href + "/albums";
+		$artistLink.attr('data-url', albumsUrl);
+		$artistLink.html(artistInfo.name);
+		$artistItem.html($artistLink);
+		// $artistLink.on('click', getAlbums);
 		$('#artist-info').append($artistItem);
 	}
 
@@ -30,7 +54,29 @@ $(document).ready(function(){
 		});
 	}
 
+	function createAlbum(album){
+		var $albumItem = $('<li>');
+		var $albumLink = $('<a href="#">').addClass('album-result');
+		$albumLink.html(album.name);
+		$albumLink.attr('data-url', album.href)
+		$albumItem.html($albumLink);
+		$('#artist-albums').append($albumItem);
+	}
+
+	function printAlbums(albums){
+		albums.forEach(function(album){
+			createAlbum(album);
+		});
+
+	}
+
+	function getTracks(event){
+		debugger
+	}
+
 
 	$("#search-button").on("click",searchArtist);
+	$('#artist-info').on('click', '.artist-result', getAlbums);
+	$('#artist-albums').on('click', '.album-result', getTracks);
 
 });
