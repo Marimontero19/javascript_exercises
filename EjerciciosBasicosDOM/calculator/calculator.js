5a15e8b2411bbc4f99123c81e7e57070a61c4e69
Maria Jose Montero <marimontero_19@hotmail.com>
2015-11-07 13:13:27 +0100

    Ejercicio calculator

--- EjerciciosBasicosDOM/calculator/calculator.js
+++ EjerciciosBasicosDOM/calculator/calculator.js
@@ -14,21 +14,29 @@ function createSelect() {
 	var select = $("<select class='select-input'></select>");
 	var sumOption = $("<option value='+'> + </option>");
 	var subtractOption = $("<option value='-'> - </option>");
-	select.append(sumOption).append(subtractOption);
-
+	var multiplyOption = $("<option value='*'> x </option>");
+	var divisionOption = $("<option value='/'> % </option>");
+	select.append(sumOption).append(subtractOption).append(multiplyOption).append(divisionOption);
 	return select;
 }
 
 function calculate(){
+	$('#container p').remove();
 	var sum = $("<p>");
 	$("#container").append(sum);
 	var inputVal1 = Number($("#input-1").val());
 	var inputVal2 = Number($("#input-2").val());
 	var operation = $(".select-input").val();
+	$('#input-1').val("");
+	$('#input-2').val("");
 	if (operation == '+'){
 		var inputSum = inputVal1 + inputVal2;
-	} else {
+	} else if (operation == '-'){
 		var inputSum = inputVal1 - inputVal2;
+	} else if (operation == '*'){
+		var inputSum = inputVal1 * inputVal2;
+	} else if (operation == '/'){
+		var inputSum = inputVal1 / inputVal2;
 	}
 	sum.append(inputSum);
 }
