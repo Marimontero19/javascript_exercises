9f67209543a3151aab0ab2907e92f52551f4b88c
Maria Jose Montero <marimontero_19@hotmail.com>
2015-11-09 21:35:55 +0100

    nov9

---
+++ EjerciciosBasicosDOM/count_letters/letters.js
@@ -0,0 +1,50 @@
+
+$(document).on('ready', function() {
+
+	function putInput(){
+		$("#container").html("<input type='text' id='input-1'>");
+		$("#container").append("<button class='submit-button'>Submit</button>");
+		$('.submit-button').on('click', countWords);
+	}
+
+	function countWords() {
+		var inputVal = $('#input-1').val();
+		$('#input-1').val('');
+		var lettersDict = countLetters(inputVal);
+		printLetters(lettersDict);
+		
+	}
+
+	function countLetters(val){
+		var solution = {}
+
+		var letters = val.split('');
+
+		letters.forEach(function(letter) {
+			if (letter == ' ') return;
+
+			var letterNumber = solution[letter];
+			if (letterNumber >= 1){
+				solution[letter] += 1
+			} else {
+				solution[letter] = 1;
+			}
+	
+		});
+		return solution;
+	}
+
+	function printLetters(solution){
+		var $list = $('<ul>');
+		$('#container').append($list);
+		for(var letter in solution){
+			var $listElement = $('<li>');
+			$list.append($listElement);
+			var number = solution[letter];
+			$listElement.append(letter + ':' + number);
+		}
+	}
+
+	putInput();
+
+});
